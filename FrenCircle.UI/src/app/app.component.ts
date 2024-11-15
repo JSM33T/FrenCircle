36686656d076f016e641123a62d988c9864adcd2
jsm33t <jskainthofficial@gmail.com>
2024-11-16 00:12:55 +0530

    test

--- FrenCircle.UI/src/app/app.component.ts
+++ FrenCircle.UI/src/app/app.component.ts
@@ -1,31 +1,40 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { Component, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterModule,
+} from '@angular/router';
 import { filter } from 'rxjs';
-import { BackToTopComponent } from "./components/shared/back-to-top/back-to-top.component";
+import { NgClass } from '@angular/common';
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterModule, BackToTopComponent],
-  template: `
-	<main class="page-wrapper">
-		<app-navbar></app-navbar>
-		<router-outlet></router-outlet>
-		<app-back-to-top></app-back-to-top>
-	</main>
-	`
+  imports: [RouterModule, NgClass, NavbarComponent],
+  templateUrl:  './app.component.html'
 })
-export class AppComponent {
-  constructor(private router: Router,) {
-    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
-      if (typeof window !== 'undefined') {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        });
-      }
-      console.log("route changed");
-    });
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (typeof window !== 'undefined') {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+        }
+        this.isHidden = true;
+        console.log('route changed');
+      });
+  }
+
+  isHidden: boolean = true;
+
+  ngOnInit(): void {}
+
+  toggleNavMenu() {
+    this.isHidden = !this.isHidden;
   }
 }
