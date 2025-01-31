4bcca13a50677bc6ce194021d55a6f87b11acb78
jsm33t <jskainthofficial@gmail.com>
2025-01-31 23:46:24 +0530

    monor changes for verification

---
+++ FrenCircle.Validatiors/AddUserRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FrenCircle.Entities.Data;
+
+namespace FrenCircle.Validators
+{
+    public class AddMessageRequestValidators : AbstractValidator<AddMessageRequest>
+    {
+        public AddMessageRequestValidators()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Please provide a valid email address.");
+
+            RuleFor(x => x.Text)
+                .NotEmpty().WithMessage("Message is required.")
+                .MinimumLength(3).WithMessage("Message too short");
+        }
+    }
+}
