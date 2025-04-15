e342b81f2d5e9f16275c0117485603bcb900c6d6
jsm33t <jskainthofficial@gmail.com>
2025-04-15 17:17:51 +0530

    :art: append new styles

--- FrenCircle.Ui/src/app/layout.tsx
+++ FrenCircle.Ui/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 import BootstrapSetup from "@/components/BootstrapSetup";
 import Navbar from "@/components/ui/Navbar";
